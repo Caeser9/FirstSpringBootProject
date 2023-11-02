cef7fde442fa13e4e127c9a139fcf1fe0b587e88
Caeser9 <kaycerkhouini86@outlook.fr>
2023-11-02 10:08:27 +0100

    Services+controllers

---
+++ src/main/java/tn/esprit/firstspringbootproject/services/ChambreServiceImpl.java
@@ -0,0 +1,43 @@
+package tn.esprit.firstspringbootproject.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tn.esprit.firstspringbootproject.entities.Chambre;
+import tn.esprit.firstspringbootproject.repository.IChambreRepository;
+
+import java.util.List;
+
+@Service
+
+public class ChambreServiceImpl implements IChambreService {
+    @Autowired
+    IChambreRepository iChambreRepository;
+    @Override
+    public List<Chambre> retrieveAllChambres() {
+        return iChambreRepository.findAll();
+    }
+
+    @Override
+    public Chambre retrieveChambre(Long chambreId) {
+
+        return iChambreRepository.findById(chambreId).get();
+    }
+
+    @Override
+    public Chambre addChambre(Chambre c) {
+
+        return iChambreRepository.save(c);
+    }
+
+    @Override
+    public void removeChambre(Long chambreId) {
+        iChambreRepository.deleteById(chambreId);
+
+    }
+
+    @Override
+    public Chambre modifyChambre(Chambre chambre) {
+
+        return iChambreRepository.save(chambre) ;
+    }
+}
